c6c1a1a38b2718bea2d4bfae06f02e4d9b576907
justai-net <adam.int@live.com>
2018-09-25 00:39:28 +1000

    gratitude packaged

---
+++ gratitude/gratitude.go
@@ -0,0 +1,39 @@
+package gratitude
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+)
+
+type quotes struct {
+	Author string
+	Quote  string
+}
+
+func ShowGratitudeQuote() {
+	csvFile, _ := os.Open("quotesofgratitude.csv")
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+	var listquotes []quotes
+	for {
+		line, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		listquotes = append(listquotes, quotes{
+			Author: line[1],
+			Quote:  line[0],
+		})
+	}
+	rand.Seed(time.Now().Unix())
+	n := rand.Int() % len(listquotes)
+	message := listquotes[n]
+	fmt.Println(message.Quote, "-", message.Author)
+}
